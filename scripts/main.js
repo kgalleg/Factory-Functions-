1a614d7741fb1308f77cb45ed33690ec395b54d8
Karla Gallegos <kgalleg618@gmail.com>
2019-06-19 15:47:40 -0500

    More practice exercises from Factory Functions Chapter

--- scripts/main.js
+++ scripts/main.js
@@ -18,20 +18,20 @@ const createDoctor = (docName, specialty, docAddress) => {
 // Pet breed
 // Invoke the factory function 3 times and place each animal in an array stored in a variable named BowWowKennels
 
-const bowWowKennels = []
-const createPet = (thisIsPetName, thisIsPetBreed) => {
-    return {
-        "petName": thisIsPetName,
-        "petBreed": thisIsPetBreed
-    }
-}
+// const bowWowKennels = []
+// const createPet = (thisIsPetName, thisIsPetBreed) => {
+//     return {
+//         "petName": thisIsPetName,
+//         "petBreed": thisIsPetBreed
+//     }
+// }
 
-const pet1 = createPet("BeeBee", "Poodle")
-const pet2 = createPet("Abby", "Labradoodle")
-const pet3 = createPet("GG", "Golden Retriever")
+// const pet1 = createPet("BeeBee", "Poodle")
+// const pet2 = createPet("Abby", "Labradoodle")
+// const pet3 = createPet("GG", "Golden Retriever")
 
-bowWowKennels.push(pet1, pet2, pet3);
-console.log(bowWowKennels)
+// bowWowKennels.push(pet1, pet2, pet3);
+// console.log(bowWowKennels)
 
 
 
@@ -43,34 +43,82 @@ console.log(bowWowKennels)
 // * Make `contactFactory` take a single argument, which will be treated as an array.
 // * In the body of the function, loop over that array to set the values of the object's properties.
 
-// ```function contactFactory(contactData) {
-//   let contact = {
-//     name: null,
-//     title: null,
-//     email: null
-//   }
-//   for (let i=0; i < contactData.length; i++) {
-//      ///add each arry item to contact
-//    }
-//   return contact
-// }```
+
+
+const contactFactory = (contactData) => {
+    let contact = {
+      name: contactData[0],
+      title: contactData[1],
+      email: contactData[2]
+    }
+    return contact;
+  }
+
 // 2. Complete the second function called `createContact`:
 // * createContact takes three arguments: name, title, email.
 // * Have the function place the three arguments into an array and pass that array to the `contactFactory` by calling it.
 
-// ```function createContact(name, title, email) {
-//   let newContact = /// pass name, title, email to contactFactory as an array
-//   console.table(Contact)
-// }```
-// 3. Execute createContact and pass in a name, a title, and an email address. Check out your dev tools console for the resulting table of data.
-
-
-const createAmplifier = function (volume = 11, color = "Black") {
-    return {
-        "type": "Guitar Amplifier",
-        "maximumVolume": volume,
-        "caseColor": color
+    function createContact(name, title, email) {
+      let newContact = [name, title, email]
+      /// pass name, title, email to contactFactory as an array
+      const contact = contactFactory(newContact);
+      console.table(contact)
     }
-}
 
-console.log(createAmplifier());
+// 3. Execute createContact and pass in a name, a title, and an email address. Check out your dev tools console for the resulting table of data.
+createContact("Karla", "Student", "kgalleg@gmail.com" )
+
+console.log(contactFactory);
+
+
+
+
+
+// const createAmplifier = function (volume = 11, color = "Black") {
+//     return {
+//         "type": "Guitar Amplifier",
+//         "maximumVolume": volume,
+//         "caseColor": color
+//     }
+// }
+
+// console.log(createAmplifier());
+
+
+
+
+
+
+// Didn't know what this exercise was asking me to do: 
+// practice: Music Row on Chapter on Factory FUnctions 
+
+// Your job is to sign each of these promising young music stars to the appropriate label.
+
+// JumpStop Records works with Funk and Rap artists.
+// Chatten Records works with Country and Bluegrass artists.
+// Polar Records works with Pop artists.
+// Create an array for each of these record labels.
+
+const recordLabels = ["JumpStop", "Chatten Records","Polar Records"]
+
+// Create a factory function for each possible genre (e.g. createBluegrassArtist()). Then invoke the appropriate function for each of the following artists and place the resulting object in the corresponding label array.
+
+// const createGenre = (genre) => {
+// return {
+//     country:
+//     pop: 
+//     funk:
+//     rap:
+//     bluegrass:
+
+//     }
+// }
+
+// Bruce Atikins is a Country artist and is 23 years old
+// Jensen Brown is a Pop artist and is 20 years old
+// Dre Funkz is a Funk artist and is 25 years old
+// Dusta Grimes is a Rap artist and is 21 years old
+// Bartholomew Danielson is a Bluegrass artist and is 23 years old
+// Avilee Dallas is a Country artist and is 19 years old
+// Austin Kinkaid is a Pop artist and is 22 years old
+// Loyoncé Branis is a Rap artist and is 27 years old
